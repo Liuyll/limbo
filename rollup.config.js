8e5ca7eb8bc6df15003c0a7d4c053f5162bcf2f6
liuyll <540374501@qq.com>
2020-12-08 11:02:56 +0800

    feat: 提供默认构建配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,7 @@
 export default {
-    input: 'src/index'
+    input: 'src/index',
+    output: [
+        { name: 'limbo', file: 'lib/limbo.js', format: 'cjs', sourcemap: true },
+        { name: 'limbo', file: 'lib/limbo.esm.js', format: 'umd', sourcemap: true }
+    ]
 }
