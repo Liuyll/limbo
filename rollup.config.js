6e2a68554d4d92f6c62f25cbaed69b39a256f1ee
liuyll <540374501@qq.com>
2021-01-25 19:50:56 +0800

    fix: 增加rollup output.exports配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,18 @@
 import { terser } from 'rollup-plugin-terser'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
 
 export default {
     input: 'src/index',
     output: [
-        { name: 'limbo', file: 'lib/limbo.js', format: 'cjs', sourcemap: true },
-        { name: 'limbo', file: 'lib/limbo.esm.js', format: 'umd', sourcemap: true },
-        { name: 'limbo', file: 'lib/limbo.min.js', format: 'cjs', plugins: [terser()], sourcemap: false }
+        { name: 'limbo', file: 'lib/limbo.js', format: 'cjs', sourcemap: true, exports: 'named' },
+        { name: 'limbo', file: 'lib/limbo.esm.js', format: 'umd', sourcemap: true, exports: 'named' },
+        { name: 'limbo', file: 'lib/limbo.min.js', format: 'cjs', plugins: [terser()], sourcemap: false, exports: 'named' }
+    ],
+    plugins: [
+        resolve(),
+        commonjs({
+            include: 'node_modules/**'
+        })
     ]
 }
